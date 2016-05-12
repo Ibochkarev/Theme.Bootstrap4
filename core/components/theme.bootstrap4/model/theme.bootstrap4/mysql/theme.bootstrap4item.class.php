34ffa8e16bca628b0482567a8154fdf566f0e080
ibochkarev <45iudyuh>
2016-05-12 23:18:54 +0400

    Add CSS, JS + change transport setings

--- core/components/theme.bootstrap4/model/theme.bootstrap4/mysql/theme.bootstrap4item.class.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once (dirname(dirname(__FILE__)) . '/theme.bootstrap4item.class.php');
-class Theme.Bootstrap4Item_mysql extends Theme.Bootstrap4Item {}
--- core/components/theme.bootstrap4/model/theme.bootstrap4/theme.bootstrap4item.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * @package theme.bootstrap4
- */
-class Theme.Bootstrap4Item extends xPDOSimpleObject {
-}
